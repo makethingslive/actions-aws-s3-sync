6532a8144d0f3c55f070ba3a90e35e344831dece
Julian <juliantellezmendez@gmail.com>
2020-10-11 20:04:43 +0100

    fix: remove dry run

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,24 +48,23 @@ if [ -n "$AWS_S3_ENDPOINT_URL" ]; then
     AWS_S3_ENDPOINT_ARG="--endpoint-url $AWS_S3_ENDPOINT_URL"
 fi
 
-if [ -z "$AWS_S3_DELETE" ]; then
-    AWS_S3_DELETE="false"
-fi
+# if [ -z "$AWS_S3_DELETE" ]; then
+#     AWS_S3_DELETE="false"
+# fi
 
-if [ $AWS_S3_DELETE = "true" ]; then
-    echo "[ WARNING ] Files that exist in the destination but not in the source will be deleted during sync."
-    AWS_S3_DELETE_ARG="--delete true"
-fi
+# if [ $AWS_S3_DELETE = "true" ]; then
+#     echo "[ WARNING ] Files that exist in the destination but not in the source will be deleted during sync."
+#     AWS_S3_DELETE_ARG="--delete true"
+# fi
 
-if [ -z "$AWS_S3_DRY_RUN" ]; then
-    AWS_S3_DRY_RUN="false"
-fi
+# if [ -z "$AWS_S3_DRY_RUN" ]; then
+#     AWS_S3_DRY_RUN="false"
+# fi
 
-if [ $AWS_S3_DRY_RUN = "true" ]; then
-    echo "[ LOG ] Dry Run ..."
-    AWS_S3_DRY_RUN_ARG="--dryrun"
-fi
-
+# if [ $AWS_S3_DRY_RUN = "true" ]; then
+#     echo "[ LOG ] Dry Run ..."
+#     AWS_S3_DRY_RUN_ARG="--dryrun"
+# fi
 
 AWS_PROFILE="temp-profile"
 AWS_S3_URI="s3://${AWS_S3_BUCKET}/${AWS_S3_DEST_DIR}"
@@ -82,8 +81,6 @@ EOF
 sh -c "aws s3 sync ${LOCAL_PATH} ${AWS_S3_URI} \
         --profile ${AWS_PROFILE} \
         --only-show-errors \
-        ${AWS_S3_DRY_RUN_ARG} \
-        ${AWS_S3_DELETE_ARG} \
 ${AWS_S3_ENDPOINT_ARG} $*"
 
 
